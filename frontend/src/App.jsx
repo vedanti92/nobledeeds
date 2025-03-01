ecbbe1ec8b5f3d8b4ca17ab8107b2a837ba54545
vedanti <vedanti.r.bagade@gmail.com>
2025-03-01 18:41:38 +0530

    file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Carousel from "./components/Carousel";
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Carousel />} />
       </Routes>

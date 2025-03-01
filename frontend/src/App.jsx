426028d93194ec7599cea74b899ee58b537c6a6e
vedanti <vedanti.r.bagade@gmail.com>
2025-03-01 17:51:18 +0530

    file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Carousel from "./components/Carousel";
+import "./App.css";
 
 function App() {
-
   return (
-    <>
-      
-    </>
-  )
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Carousel />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/Carousel.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import "./Carousel.css";
+
+function Carousel() {
+  return (
+    <div
+      id="carouselExampleAutoplaying"
+      className="carousel slide carousel-fade mt-3"
+      data-bs-ride="carousel"
+    >
+      <div className="carousel-inner">
+        <div className="carousel-item active">
+          <img src="/images/img1.png" className="d-block w-100" alt="image" />
+        </div>
+        <div className="carousel-item">
+          <img src="/images/img2.png" className="d-block w-100" alt="image" />
+        </div>
+        <div className="carousel-item">
+          <img src="/images/img3.png" className="d-block w-100" alt="image" />
+        </div>
+      </div>
+      <button
+        className="carousel-control-prev icon"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="prev"
+      >
+        <i className="fa-solid fa-chevron-left"></i>
+      </button>
+      <button
+        className="carousel-control-next icon"
+        type="button"
+        data-bs-target="#carouselExampleAutoplaying"
+        data-bs-slide="next"
+      >
+        <i className="fa-solid fa-chevron-right"></i>
+      </button>
+    </div>
+  );
+}
+
+export default Carousel;

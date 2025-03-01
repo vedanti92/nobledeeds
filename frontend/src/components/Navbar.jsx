697818a0ca7ca79516e7025a2f31c4b702738cef
vedanti <vedanti.r.bagade@gmail.com>
2025-03-01 19:03:50 +0530

    file updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -13,7 +13,7 @@ import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
 
-const pages = ["Campaigns", "Pricing", "Blog"];
+const pages = ["Home", "Add Campaigns"];
 const settings = ["Account", "Logout"];
 
 function Navbar() {

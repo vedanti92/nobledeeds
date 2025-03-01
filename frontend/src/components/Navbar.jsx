ff60ad37bb2de17aee7d208fd919f8b711d304e6
vedanti <vedanti.r.bagade@gmail.com>
2025-03-01 18:43:24 +0530

    file updated

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -125,7 +125,7 @@ function Navbar() {
           <Box sx={{ flexGrow: 0 }}>
             <Tooltip title="Open settings">
               <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Profile" src="/static/images/avatar/2.jpg" />
+                <Avatar alt="Profile" src="/images/user.png" />
               </IconButton>
             </Tooltip>
             <Menu

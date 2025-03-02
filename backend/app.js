0c0576b9a2a7e628178a0aa7efdb00d270516bcd
vedanti <vedanti.r.bagade@gmail.com>
2025-03-02 16:41:37 +0530

    file updated

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+const Campaign = require("./models/campaigns");
 require("dotenv").config();
 const mongoose = require("mongoose");
 const express = require("express");
@@ -11,6 +12,17 @@ const PORT = 8080;
 
 app.use(cors());
 app.use(bodyParser.json());
+
+app.get("/home", async (req, res) => {
+  const { category } = req.query;
+  let campaigns;
+  if (category && category !== "all") {
+    campaigns = await Campaign.find({ category });
+  } else {
+    campaigns = await Campaign.find({});
+  }
+  res.json(campaigns);
+});
 
 app.listen(PORT, () => {
   console.log(`server listening on ${PORT}`);

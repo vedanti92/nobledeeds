c8a4c71ad9a071bee0feed3adb486109f9597478
vedanti <vedanti.r.bagade@gmail.com>
2025-03-02 19:42:37 +0530

    file updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
 import axios from "axios";
-import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid2";
 import Carousel from "../components/Carousel";
 import Categories from "../components/Categories";
@@ -21,23 +21,39 @@ export default Home;
 
 const Campaigns = () => {
   const [campaigns, setCampaigns] = useState([]);
+  const [category, setCategory] = useState("all");
+  const [searchParams] = useSearchParams();
+  const selectedCategory = searchParams.get("category") || "all";
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/home")
+      .get("http://localhost:8080/home", {
+        params: { category: selectedCategory },
+      })
       .then((res) => setCampaigns(res.data))
       .catch((err) => console.error("Error fetching campaigns:", err));
   }, []);
 
+  const filteredCampaigns =
+    category === "all"
+      ? campaigns
+      : campaigns.filter((c) => c.category === category);
+
   return (
-    <Container sx={{ mt: 4 }}>
-      <Grid container spacing={3} justifyContent="center">
-        {campaigns.map((campaign) => (
-          <Grid item xs={12} sm={6} md={4} key={campaign._id}>
-            <CampaignCard campaign={campaign} />
-          </Grid>
-        ))}
-      </Grid>
-    </Container>
+    <>
+      <Container sx={{ mt: 4 }}>
+        <Grid container spacing={3} justifyContent="center">
+          {filteredCampaigns.length > 0 ? (
+            filteredCampaigns.map((campaign) => (
+              <Grid item xs={12} sm={6} md={4} key={campaign._id}>
+                <CampaignCard campaign={campaign} />
+              </Grid>
+            ))
+          ) : (
+            <h6>No campaigns found for this category.</h6>
+          )}
+        </Grid>
+      </Container>
+    </>
   );
 };

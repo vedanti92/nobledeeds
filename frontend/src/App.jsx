7a54eb60b56d5a8ad7ddfddcdcbcebe80392ee3d
vedanti <vedanti.r.bagade@gmail.com>
2025-03-04 13:10:15 +0530

    file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import CampaignDetails from "./pages/CampaignDetails";
 import SearchResults from "./pages/SearchResults";
+import AddCampaign from "./pages/AddCampaign";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/search" element={<SearchResults />} />
+        <Route path="/addCampaign" element={<AddCampaign />} />
         <Route path="/:id" element={<CampaignDetails />} />
       </Routes>
     </BrowserRouter>

f421e83154d497c4b650bd536657efd45e1ef5e6
vedanti <vedanti.r.bagade@gmail.com>
2025-03-06 20:49:42 +0530

    file updated

--- frontend/src/pages/AddCampaign.jsx
+++ frontend/src/pages/AddCampaign.jsx
@@ -7,6 +7,10 @@ function AddCampaign() {
       style={{ marginTop: "80px", marginLeft: "200px", marginRight: "200px" }}
     >
       <h2>Add Campaign</h2>
+      <form action="">
+        <label htmlFor="title">Title</label> <br />
+        <input type="text" name="title" id="title" />
+      </form>
     </div>
   );
 }

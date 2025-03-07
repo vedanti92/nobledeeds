f4031dac5c1f2c719bef74de8c22a2f5d4a6f7e1
vedanti <vedanti.r.bagade@gmail.com>
2025-03-07 11:22:50 +0530

    file updated

--- frontend/src/pages/AddCampaign.jsx
+++ frontend/src/pages/AddCampaign.jsx
@@ -7,10 +7,80 @@ function AddCampaign() {
       <div className="col-6" style={{ marginTop: "80px" }}>
         <h2>Add Campaign</h2>
         <form action="">
-          <label htmlFor="title" className="form-label">
-            Title
-          </label>
-          <input type="text" name="title" id="title" className="form-control" />
+          <div className="mb-3">
+            <label htmlFor="title" className="form-label">
+              Title
+            </label>
+            <input
+              type="text"
+              name="title"
+              id="title"
+              placeholder="Add a title for your campaign"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="description" className="form-label">
+              Description
+            </label>
+            <textarea
+              type="text"
+              name="description"
+              id="description"
+              placeholder="Add description"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="image" className="form-label">
+              Image
+            </label>
+            <input
+              type="text"
+              name="image"
+              id="image"
+              placeholder="Add an image URL"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="goalAmount" className="form-label">
+              Goal Amount
+            </label>
+            <input
+              type="text"
+              name="goalAmount"
+              id="goalAmount"
+              placeholder="Add total amount required"
+              className="form-control"
+            />
+          </div>
+
+          <div className="mb-3">
+            <label htmlFor="category" className="form-label">
+              Category
+            </label>
+            <select name="category" id="category" className="form-control">
+              <option value="" disabled selected>
+                Select a category
+              </option>
+              <option value="all">All</option>
+              <option value="disaster-relief">Disaster Relief</option>
+              <option value="food-assistance">Food Assistance</option>
+              <option value="education">Education</option>
+              <option value="animals">Animals</option>
+              <option value="children">Children</option>
+              <option value="senior-care">Senior Care</option>
+              <option value="women">Women</option>
+              <option value="specially-abled">Specially Abled</option>
+              <option value="medical-aid">Medical Aid</option>
+              <option value="environment">Environment</option>
+              <option value="others">Others</option>
+            </select>
+          </div>
         </form>
       </div>
       <div className="col-3"></div>

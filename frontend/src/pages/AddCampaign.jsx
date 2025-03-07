668ad5bc318a1d07cfe60f90e3ed24c6131c749a
vedanti <vedanti.r.bagade@gmail.com>
2025-03-07 11:24:26 +0530

    file updated

--- frontend/src/pages/AddCampaign.jsx
+++ frontend/src/pages/AddCampaign.jsx
@@ -46,40 +46,42 @@ function AddCampaign() {
             />
           </div>
 
-          <div className="mb-3">
-            <label htmlFor="goalAmount" className="form-label">
-              Goal Amount
-            </label>
-            <input
-              type="text"
-              name="goalAmount"
-              id="goalAmount"
-              placeholder="Add total amount required"
-              className="form-control"
-            />
-          </div>
+          <div className="row">
+            <div className="col-6 mb-3">
+              <label htmlFor="goalAmount" className="form-label">
+                Goal Amount
+              </label>
+              <input
+                type="text"
+                name="goalAmount"
+                id="goalAmount"
+                placeholder="Add total amount required"
+                className="form-control"
+              />
+            </div>
 
-          <div className="mb-3">
-            <label htmlFor="category" className="form-label">
-              Category
-            </label>
-            <select name="category" id="category" className="form-control">
-              <option value="" disabled selected>
-                Select a category
-              </option>
-              <option value="all">All</option>
-              <option value="disaster-relief">Disaster Relief</option>
-              <option value="food-assistance">Food Assistance</option>
-              <option value="education">Education</option>
-              <option value="animals">Animals</option>
-              <option value="children">Children</option>
-              <option value="senior-care">Senior Care</option>
-              <option value="women">Women</option>
-              <option value="specially-abled">Specially Abled</option>
-              <option value="medical-aid">Medical Aid</option>
-              <option value="environment">Environment</option>
-              <option value="others">Others</option>
-            </select>
+            <div className="col-6 mb-3">
+              <label htmlFor="category" className="form-label">
+                Category
+              </label>
+              <select name="category" id="category" className="form-control">
+                <option value="" disabled selected>
+                  Select a category
+                </option>
+                <option value="all">All</option>
+                <option value="disaster-relief">Disaster Relief</option>
+                <option value="food-assistance">Food Assistance</option>
+                <option value="education">Education</option>
+                <option value="animals">Animals</option>
+                <option value="children">Children</option>
+                <option value="senior-care">Senior Care</option>
+                <option value="women">Women</option>
+                <option value="specially-abled">Specially Abled</option>
+                <option value="medical-aid">Medical Aid</option>
+                <option value="environment">Environment</option>
+                <option value="others">Others</option>
+              </select>
+            </div>
           </div>
         </form>
       </div>

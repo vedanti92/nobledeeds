b7ccb74a5f424a4c982b2fa55414a890dba95ef0
vedanti <vedanti.r.bagade@gmail.com>
2025-03-09 12:40:38 +0530

    file updated

--- frontend/src/components/Categories.jsx
+++ frontend/src/components/Categories.jsx
@@ -32,40 +32,40 @@ function Categories() {
             className="form-control"
           >
             <MenuItem value="all" className="category">
-              <i class="fa-solid fa-layer-group"></i>All
+              <i class="fa-solid fa-layer-group"></i> &nbsp; All
             </MenuItem>
             <MenuItem value="disaster-relief" className="category">
-              <i class="fa-solid fa-house-crack"></i>Disaster Relief
+              <i class="fa-solid fa-house-crack"></i> &nbsp; Disaster Relief
             </MenuItem>
             <MenuItem value="food-assistance" className="category">
-              <i class="fa-solid fa-utensils"></i>Food Assistance
+              <i class="fa-solid fa-utensils"></i> &nbsp; Food Assistance
             </MenuItem>
             <MenuItem value="education" className="category">
-              <i class="fa-solid fa-chalkboard-user"></i>Education
+              <i class="fa-solid fa-chalkboard-user"></i> &nbsp; Education
             </MenuItem>
             <MenuItem value="animals" className="category">
-              <i class="fa-solid fa-paw"></i>Animals
+              <i class="fa-solid fa-paw"></i> &nbsp; Animals
             </MenuItem>
             <MenuItem value="children" className="category">
-              <i class="fa-solid fa-child"></i>Children
+              <i class="fa-solid fa-child"></i> &nbsp; Children
             </MenuItem>
             <MenuItem value="senior-care" className="category">
-              <i class="fa-solid fa-person-cane"></i>Senior Care
+              <i class="fa-solid fa-person-cane"></i> &nbsp; Senior Care
             </MenuItem>
             <MenuItem value="specially-abled" className="category">
-              <i class="fa-solid fa-wheelchair"></i>Specially Abled
+              <i class="fa-solid fa-wheelchair"></i> &nbsp; Specially Abled
             </MenuItem>
             <MenuItem value="medical-aid" className="category">
-              <i class="fa-solid fa-suitcase-medical"></i>Medical Aid
+              <i class="fa-solid fa-suitcase-medical"></i> &nbsp; Medical Aid
             </MenuItem>
             <MenuItem value="women" className="category">
-              <i class="fa-solid fa-person-dress"></i>Women
+              <i class="fa-solid fa-person-dress"></i> &nbsp; Women
             </MenuItem>
             <MenuItem value="environment" className="category">
-              <i class="fa-solid fa-seedling"></i>Environment
+              <i class="fa-solid fa-seedling"></i> &nbsp; Environment
             </MenuItem>
             <MenuItem value="others" className="category">
-              <i class="fa-solid fa-ellipsis"></i>Others
+              <i class="fa-solid fa-ellipsis"></i> &nbsp; Others
             </MenuItem>
           </Select>
         </FormControl>

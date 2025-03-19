32e409f29651d7b643661b2ce9980239c5025d55
vedanti <vedanti.r.bagade@gmail.com>
2025-03-19 19:22:37 +0530

    file updated

--- frontend/src/pages/AddCampaign.jsx
+++ frontend/src/pages/AddCampaign.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
+import "./AddCampaign.css";
 
 function AddCampaign() {
   const navigate = useNavigate();

794a46c0a0e6d28cd93ecffcea367ce1d0c50c85
vedanti <vedanti.r.bagade@gmail.com>
2025-03-20 19:06:07 +0530

    file updated

---
+++ backend/models/users.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const { required } = require("joi");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+userSchema.pre("save", async function () {
+  this.password = await bcrypt.hash(this.password, 12);
+});
+
+module.exports = mongoose.model("User", userSchema);

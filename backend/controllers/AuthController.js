907bee4934363447397b87b44fe65c1aeb6bf29f
vedanti <vedanti.r.bagade@gmail.com>
2025-03-21 19:11:01 +0530

    file updated

---
+++ backend/controllers/AuthController.js
@@ -0,0 +1,25 @@
+const User = require("../models/users");
+const { createSecretToken } = require("../utils/SecretToken");
+const bcrypt = require("bcrypt");
+
+module.exports.Signup = async (req, res, next) => {
+  try {
+    const { email, username, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, username, password });
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed in successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};

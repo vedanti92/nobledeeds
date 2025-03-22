678eb4c678686358c14131fc5fcc230e75967797
vedanti <vedanti.r.bagade@gmail.com>
2025-03-22 15:59:13 +0530

    file updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Login = () => {
-  return <div>Login Page</div>;
+  return <div style={{ marginTop: "100px" }}>Login Page</div>;
 };
 
 export default Login;
--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Signup = () => {
-  return <div>Signup Page</div>;
+  return <div style={{marginTop: "100px"}}>Signup Page</div>;
 };
 
 export default Signup;

dbb2507ba77e275f0b43b7caa81ac304e427f618
vedanti <vedanti.r.bagade@gmail.com>
2025-03-22 11:40:50 +0530

    file updated

--- backend/routes/AuthRoute.js
+++ backend/routes/AuthRoute.js
@@ -1,6 +1,7 @@
-const { Signup } = require("../controllers/AuthController");
+const { Signup, Login } = require("../controllers/AuthController");
 const router = require("express").Router();
 
 router.post("/signup", Signup);
+router.post("/login", Login);
 
 module.exports = router;

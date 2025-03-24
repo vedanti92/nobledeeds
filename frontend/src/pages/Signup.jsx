b212feee5db90eebb02eb82a4930f2208ee6de89
vedanti <vedanti.r.bagade@gmail.com>
2025-03-24 20:06:13 +0530

    file updated

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,7 +1,25 @@
 import React from "react";
 
 const Signup = () => {
-  return <div style={{marginTop: "100px"}}>Signup Page</div>;
+  return (
+    <div className="container">
+      <h2>Signup</h2>
+      <form>
+        <div>
+          <label className="form-label" htmlFor="email">Email</label>
+          <input className="form-control" type="email" placeholder="abc@gmail.com" name="email" />
+        </div>
+        <div>
+          <label className="form-label" htmlFor="username">Username</label>
+          <input className="form-control" type="text" placeholder="Username" name="username" />
+        </div>
+        <div>
+          <label className="form-label" htmlFor="password">Password</label>
+          <input className="form-control" type="password" placeholder="********" name="password" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default Signup;

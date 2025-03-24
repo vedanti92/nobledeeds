08f33da5b78fe6ac7c8b8af1af49b12cfcea971f
vedanti <vedanti.r.bagade@gmail.com>
2025-03-24 20:13:00 +0530

    file updated

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -1,21 +1,44 @@
 import React from "react";
+import "./Signup.css";
 
 const Signup = () => {
   return (
     <div className="container">
       <h2>Signup</h2>
+      <br />
       <form>
         <div>
-          <label className="form-label" htmlFor="email">Email</label>
-          <input className="form-control" type="email" placeholder="abc@gmail.com" name="email" />
+          <label className="form-label" htmlFor="email">
+            Email
+          </label>
+          <input
+            className="form-control"
+            type="email"
+            placeholder="Enter your email."
+            name="email"
+          />
         </div>
         <div>
-          <label className="form-label" htmlFor="username">Username</label>
-          <input className="form-control" type="text" placeholder="Username" name="username" />
+          <label className="form-label" htmlFor="username">
+            Username
+          </label>
+          <input
+            className="form-control"
+            type="text"
+            placeholder="Enter your username."
+            name="username"
+          />
         </div>
         <div>
-          <label className="form-label" htmlFor="password">Password</label>
-          <input className="form-control" type="password" placeholder="********" name="password" />
+          <label className="form-label" htmlFor="password">
+            Password
+          </label>
+          <input
+            className="form-control"
+            type="password"
+            placeholder="Enter your password."
+            name="password"
+          />
         </div>
       </form>
     </div>

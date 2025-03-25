76ba613f986c9c37fb702d00484ccf212b8eb5e3
vedanti <vedanti.r.bagade@gmail.com>
2025-03-25 19:01:18 +0530

    file updated

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
+import { ToastContainer, toast } from "react-toastify";
 import "./Login.css";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const [inputValue, setInputValue] = useState({
+    username: "",
+    password: "",
+  });
+
+  const { username, password } = inputValue;
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setInputValue({
+      ...inputValue,
+      [name]: value,
+    });
+  };
+
+  const handleError = (err) =>
+    toast.error(err, {
+      position: "bottom-left",
+    });
+
+  const handleSuccess = (msg) =>
+    toast.success(msg, {
+      position: "bottom-right",
+    });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.post(
+        "http://localhost:8080/login",
+        {
+          ...inputValue,
+        },
+        { withCredentials: true }
+      );
+
+      const { success, message } = data;
+      if (success) {
+        handleSuccess(message);
+        setTimeout(() => {
+          navigate("/");
+        }, 1000);
+      } else {
+        handleError(message);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+    setInputValue({
+      ...inputValue,
+      username: "",
+      password: "",
+    });
+  };
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="login-heading">
           <p>LOGIN</p>
         </div>
@@ -17,6 +76,8 @@ const Login = () => {
             type="text"
             name="username"
             id="username"
+            value={username}
+            onChange={handleOnChange}
           />
         </div>
         <br />
@@ -28,11 +89,30 @@ const Login = () => {
             type="password"
             name="password"
             id="password"
+            value={password}
+            onChange={handleOnChange}
           />
         </div>
         <br />
-        <button className="login-button">LOGIN</button>
+        <button type="submit" className="login-button">
+          LOGIN
+        </button>
+        <br />
+        <div className="mt-1" style={{ textAlign: "center" }}>
+          Don't have an account?{" "}
+          <Link
+            style={{
+              textDecoration: "none",
+              color: "#6cd4ff",
+              fontWeight: "500",
+            }}
+            to={"/signup"}
+          >
+            Signup
+          </Link>
+        </div>
       </form>
+      <ToastContainer />
     </div>
   );
 };

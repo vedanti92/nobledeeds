8b283a65f41627a2993cb235f07a6d6e20ccf8c6
vedanti <vedanti.r.bagade@gmail.com>
2025-03-25 19:22:40 +0530

    file updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,22 +9,25 @@ import EditCampaign from "./pages/EditCampaign";
 import Donation from "./pages/Donation";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import { CookiesProvider } from "react-cookie";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/search" element={<SearchResults />} />
-        <Route path="/addCampaign" element={<AddCampaign />} />
-        <Route path="/editCampaign/:id" element={<EditCampaign />} />
-        <Route path="/donate/:id" element={<Donation />} />
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/:id" element={<CampaignDetails />} />
-      </Routes>
-    </BrowserRouter>
+    <CookiesProvider>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/search" element={<SearchResults />} />
+          <Route path="/addCampaign" element={<AddCampaign />} />
+          <Route path="/editCampaign/:id" element={<EditCampaign />} />
+          <Route path="/donate/:id" element={<Donation />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/:id" element={<CampaignDetails />} />
+        </Routes>
+      </BrowserRouter>
+    </CookiesProvider>
   );
 }
 

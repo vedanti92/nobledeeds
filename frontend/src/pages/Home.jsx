fde892924e6ec1cbca8f474e66f1c5ac73fc6672
vedanti <vedanti.r.bagade@gmail.com>
2025-03-25 19:23:09 +0530

    file updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
+import { useCookies } from "react-cookie";
 import axios from "axios";
 import Grid from "@mui/material/Grid2";
 import Carousel from "../components/Carousel";
@@ -7,15 +8,46 @@ import Categories from "../components/Categories";
 import CampaignCard from "../components/CampaignCard";
 import { Container } from "@mui/material";
 import "./Home.css";
+import { toast, ToastContainer } from "react-toastify";
 
 function Home() {
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+
+      const { data } = await axios.post(
+        "http://localhost:8080",
+        {},
+        { withCredentials: true }
+      );
+
+      const { status, user } = data;
+      setUsername(user);
+      return status
+        ? toast(`Welcome ${user}!`, {
+            position: "top-right",
+          })
+        : (removeCookie("token"), navigate("/login"));
+    };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
+
   return (
     <>
-      <Carousel />
-      <div className="categories-container">
-        <Categories />
+      <div>
+        <Carousel />
+        <div className="categories-container">
+          <Categories />
+        </div>
+        <Campaigns />
       </div>
-      <Campaigns />
+      <ToastContainer />
     </>
   );
 }

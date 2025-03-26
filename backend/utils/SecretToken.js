4b0f762525d8717237b8a1637e8b2934f5677630
vedanti <vedanti.r.bagade@gmail.com>
2025-03-26 22:10:33 +0530

    file updated

--- backend/utils/SecretToken.js
+++ backend/utils/SecretToken.js
@@ -2,11 +2,8 @@ require("dotenv").config();
 const jwt = require("jsonwebtoken");
 
 module.exports.createSecretToken = (id) => {
-  console.log("Creating token for id:", id);
-  console.log("TOKEN_KEY:", process.env.TOKEN_KEY);
   const token = jwt.sign({ id }, process.env.TOKEN_KEY, {
     expiresIn: 3 * 24 * 60 * 60,
   });
-  console.log("Token created:", token);
   return token;
 };

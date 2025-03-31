1246fd0da9e66b70678bdac8153bdd69635a6d2f
vedanti <vedanti.r.bagade@gmail.com>
2025-04-01 00:20:54 +0530

    file updated

--- backend/app.js
+++ backend/app.js
@@ -13,14 +13,12 @@ const url = process.env.MONGO_URL;
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: ["http://localhost:5173"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-    exposedHeaders: ["set-cookie"],
-  })
-);
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"]
+}));
 app.use(cookieParser());
 
 app.use("/", authRoute);

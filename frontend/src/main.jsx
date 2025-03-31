26590f9c8bb5561d75a2abe88eebf5aa2b691aae
vedanti <vedanti.r.bagade@gmail.com>
2025-04-01 00:23:03 +0530

    file updated

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import axios from 'axios';
+import axios from "axios";
+import "react-toastify/dist/ReactToastify.css";
 
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = 'http://localhost:8080';
+axios.defaults.baseURL = "http://localhost:8080";
 
-createRoot(document.getElementById("root")).render(<App />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);

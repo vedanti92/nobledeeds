0eda1f17787d26847ce3d5ad63c29017f63807b5
vedanti <vedanti.r.bagade@gmail.com>
2025-04-04 18:09:14 +0530

    file updated

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./NotFound.css";
 
 const NotFound = () => {
@@ -6,7 +7,9 @@ const NotFound = () => {
     <div className="notfound-container">
       <h1>Oops!</h1>
       <p className="fs-5 mt-3">The page you're looking for does not exist!</p>
-      <button className="goback mt-5">Go back to Home</button>
+      <Link to="/">
+        <button className="goback mt-5">Go back to Home</button>
+      </Link>
     </div>
   );
 };

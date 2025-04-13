26ad1242572d2c5c087ee810a433e8f421b0de9b
vedanti <vedanti.r.bagade@gmail.com>
2025-04-13 17:49:49 +0530

    file updated

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
   return (
     <div className="notfound-container">
       <h1>Oops!</h1>
-      <p className="fs-5 mt-3">The page you're looking for does not exist!</p>
+      <p className="fs-5 mt-3" style={{textAlign: "center"}}>The page you're looking for does not exist!</p>
       <Link to="/">
         <button className="goback mt-5">Go back to Home</button>
       </Link>

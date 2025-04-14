bd669efd8e8fc38a209e056f61997534b6d2df9a
vedanti <vedanti.r.bagade@gmail.com>
2025-04-14 20:37:52 +0530

    file updated

--- backend/app.js
+++ backend/app.js
@@ -13,12 +13,14 @@ const url = process.env.MONGO_URL;
 const port = process.env.PORT;
 
 app.use(express.json());
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type"]
-}));
+app.use(
+  cors({
+    origin: "https://nobledeeds.onrender.com/",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type"],
+  })
+);
 app.use(cookieParser());
 
 app.use("/", authRoute);

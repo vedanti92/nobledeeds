1c034450244c7bded803198b374daaf5820bf600
vedanti <vedanti.r.bagade@gmail.com>
2025-04-14 21:49:17 +0530

    file updated

--- backend/app.js
+++ backend/app.js
@@ -16,10 +16,10 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://nobledeeds.onrender.com",
+    origin: ["https://nobledeeds.onrender.com", "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
-    allowedHeaders: ["Content-Type"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
--- frontend/src/pages/SearchResults.jsx
+++ frontend/src/pages/SearchResults.jsx
@@ -3,6 +3,7 @@ import { useSearchParams } from "react-router-dom";
 import { Grid, Container } from "@mui/material";
 import CampaignCard from "../components/CampaignCard";
 import "./SearchResults.css";
+import axios from "axios";
 
 function SearchResults() {
   const [searchParams] = useSearchParams();
@@ -12,10 +13,12 @@ function SearchResults() {
   React.useEffect(() => {
     const query = searchParams.get("query");
     if (query) {
-      fetch(`/search?query=${query}`)
-        .then((res) => res.json())
-        .then((data) => {
-          setResults(data);
+      axios.get(`/search`, {
+        params: { query },
+        withCredentials: true
+      })
+        .then((response) => {
+          setResults(response.data);
           setLoading(false);
         })
         .catch((error) => {
@@ -44,8 +47,8 @@ function SearchResults() {
             alignItems="center"
           >
             {results.map((campaign) => (
-              <Grid item xs={12} sm={6} md={4}>
-                <CampaignCard key={campaign._id} campaign={campaign} />
+              <Grid item xs={12} sm={6} md={4} key={campaign._id}>
+                <CampaignCard campaign={campaign} />
               </Grid>
             ))}
           </Grid>
